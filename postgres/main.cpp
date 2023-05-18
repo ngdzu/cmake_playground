35ad9df688e631499d9c493729242b6737278fec
Dzu Nguyen <dzutnguyen@gmail.com>
2023-05-18 17:09:46 -0400

    Use find_package to find postgress

---
+++ postgres/main.cpp
@@ -0,0 +1,27 @@
+#include <pqxx/pqxx>
+#include <iostream>
+int main()
+{
+  try
+  {
+    std::string connection_string = "dbname=mydatabase user=myuser password=mypassword host=localhost port=5432";
+    pqxx::connection conn(connection_string);
+    if (conn.is_open())
+    {
+      // Connection successful
+      std::cout << "Connected to PostgreSQL database" << std::endl;
+    }
+    else
+    {
+      // Connection failed
+      std::cout << "Failed to connect to PostgreSQL database" << std::endl;
+    }
+  }
+  catch (const std::exception &e)
+  {
+    // Handle connection error
+    std::cerr << "Connection error: " << e.what() << std::endl;
+  }
+
+  return 0;
+}
